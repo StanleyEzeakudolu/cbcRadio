011138db8a50369826a9dd80c523e1d21262a215
Chibuzor Ezeakudolu <stigezeh@Chibuzors-MacBook-Pro.local>
2025-01-16 12:28:02 -0500

    Scheduled time is meant to be null

--- src/main/java/com/example/cbc_vcms_internal/controllers/PostController.java
+++ src/main/java/com/example/cbc_vcms_internal/controllers/PostController.java
@@ -54,6 +54,7 @@ public class PostController {
             }
 
             if (post.getScheduledTime() == null) {
+                System.out.println("Scheduled time is meant to be null");
                 post.setScheduledTime(null);
                 post.getPlatforms().forEach(platform -> {
                     SocialMediaService service = socialMediaServices.get(platform.toLowerCase());
